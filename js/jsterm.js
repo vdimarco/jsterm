c71e1766ba9c8fd83fca8431a2cc5832fc49bed0
vdimarco <vaughn.dimarco@gmail.com>
2014-03-16 14:00:36 -0400

    content modifications

--- js/jsterm.js
+++ js/jsterm.js
@@ -1,17 +1,3 @@
-// Copyright 2013 Clark DuVall
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 (function() {
    if (typeof Object.create !== 'function') {
       Object.create = function (o) {
@@ -487,9 +473,9 @@
    var term = Object.create(Terminal);
    term.init(CONFIG, '/json/sample.json', COMMANDS, function() {
       term.enqueue('login')
-          .enqueue('clark')
+          .enqueue('vaughn')
           .enqueue('******')
-          .enqueue('cat file1')
+          .enqueue('cat README')
           .enqueue('help')
           .enqueue('ls -l')
           .enqueue('cd dir1/foo')
